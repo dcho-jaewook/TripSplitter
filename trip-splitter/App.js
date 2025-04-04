bbc2a18a938eac0b4e93367683499ecd55ce3a64
dcho-jaewook <fermatjw@gmail.com>
2025-04-04 12:53:28 -0700

    Camera Feature

--- trip-splitter/App.js
+++ trip-splitter/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, ScrollView, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import Checkbox from "expo-checkbox";
+import CameraApp from "./components/Camera";
 
 // Add these theme colors at the top, outside the App component
 const themes = {
@@ -509,6 +510,7 @@ export default function App() {
             <Text style={[styles.settledMessage, { color: theme.text }]}>All expenses have been settled! 🎉</Text>
           )}
         </View>
+        <CameraApp/>
       </View>
     </ScrollView>
   );
