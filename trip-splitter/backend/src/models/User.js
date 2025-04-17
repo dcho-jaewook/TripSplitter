a3cb700cdfd11fd6c6bff11faeca025d6c0f36c1
dcho-jaewook <fermatjw@gmail.com>
2025-04-16 20:33:26 -0700

    Backend setup

---
+++ trip-splitter/backend/src/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    // provider: { type: String, enum: ["kakao", "google", "local"] },
+    // providerId: String,
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileImage: { type: String, default:"" },
+    // trips: [{ type: mongoose.Schema.Types.ObjectId, ref: "Trip" }],
+    // expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
+    createdAt: { type: Date, default: Date.now },  
+});
+
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+})
+
+const User = mongoose.model("User", userSchema);
+export default User;
